624c5f523e36b82de28d49d966b3ba17c378395c
michal <viso.laci@gmail.com>
2015-04-12 23:40:51 +0200

    Added WebAccess module to provide a WebBrowser instance so that the bot can navigate in the page and perform clicks etc.
    Now just login.

---
+++ travianBot/ConstantsProvider.fs
@@ -0,0 +1,5 @@
+﻿module ConstantsProvider
+
+let mainUrl = "http://ts19.travian.sk/"
+
+let credentials = ("", "")
---
+++ travianBot/WebAccess.fs
@@ -0,0 +1,28 @@
+﻿module WebAccess 
+
+open System.Windows.Forms 
+
+let htmlElementCollectionToSeq (collection: HtmlElementCollection) =
+    let enumerator = collection.GetEnumerator()
+    seq {       
+          while enumerator.MoveNext () do 
+             yield enumerator.Current :?> HtmlElement 
+    }
+
+let private login (browser: WebBrowser) (callback : (unit -> unit)) =
+    let elements = htmlElementCollectionToSeq(browser.Document.GetElementsByTagName("input"))
+    let nameEl = elements |> Seq.filter(fun e -> e.GetAttribute("name").Equals("name")) |> Seq.exactlyOne
+    let passwordEl = elements |> Seq.filter(fun e -> e.GetAttribute("name").Equals("password")) |> Seq.exactlyOne
+    let (name, password) = ConstantsProvider.credentials
+    nameEl.SetAttribute("value", name)
+    passwordEl.SetAttribute("value", password)
+    0
+
+let getTravianMain (callback : (unit -> unit))  =
+    let browser = new WebBrowser()
+    browser.DocumentCompleted.Add
+        (fun _ ->  (login browser callback) |> ignore)
+    browser.Navigate(ConstantsProvider.mainUrl)
+    0
+
+    
